a7db0f03523c8c750c2f57247e8199a24b23e0e5
unknown <khss4008@gmail.com>
2023-01-16 10:11:55 +0900

    good morning

--- myproject_new/src/main/java/main/web/BoardController.java
+++ myproject_new/src/main/java/main/web/BoardController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import javax.annotation.Resource;
 
+import org.egovframe.rte.psl.dataaccess.util.EgovMap;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -26,13 +27,13 @@ public class BoardController {
 	
 	@RequestMapping("/boardWriteSave.do")
 	@ResponseBody
-	public String insertBoard(BoardVO vo) throws Exception {
-		
-		//구현체를 찾아야 하는데 그 때 위의 어노테이션에 적힌 name을 통해서 찾는 것.
+	public String insertBoard(BoardVO vo)  throws Exception{
 		String result = boardService.insertNBoard(vo);
-		String msg = "";
-		if(result == null) msg = "ok";
-		else msg = "fail";
+			
+			String msg = "";
+			if(result == null) msg = "ok";
+			else msg = "fail";
+
 		
 		return msg;
 	}
@@ -40,9 +41,10 @@ public class BoardController {
 	@RequestMapping("/boardList.do")
 	public String selectNBoardList(BoardVO vo, ModelMap model) throws Exception{
 		
-		List<?> result = boardService.selectNBoardList(vo);
-		model.addAttribute("resultList",result);
-		
+
+			List<?> result = boardService.selectNBoardList(vo);
+
+			model.addAttribute("resultList",result);
 		
 		return "board/boardList";
 	}
