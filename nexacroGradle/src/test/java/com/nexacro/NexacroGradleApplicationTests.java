2049f8f1323f204f1d4394596696a84f0e9d91f3
unknown <khss4008@gmail.com>
2023-01-25 08:48:12 +0900

    good

---
+++ nexacroGradle/src/test/java/com/nexacro/NexacroGradleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nexacro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NexacroGradleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
